575dd2c3299a90ac4bcf013c456adef36237b1e1
Vincent Gaillard <vgaillard85@gmail.com>
2025-02-03 10:31:47 +0100

    mise en place du responsive et finalisation de la page contact

--- src/components/certifications.jsx
+++ src/components/certifications.jsx
@@ -1,71 +1,133 @@
 import { motion } from "framer-motion";
 
-export function Certifications(){
-    return (
-       <> 
-        <motion.div
-        className="flex flex-wrap flex-col gap-4 p-8 rounded-[15px] border border-solid border-white w-[75%] "
+export function Certifications() {
+  return (
+    <div className="flex flex-col gap-8 p-4 lg:p-8 overflow-hidden">
+      {/* Première certification */}
+      <motion.div
+        className="flex flex-col gap-4 p-4 lg:p-8 rounded-[15px] border border-solid border-white w-full lg:w-[75%] mx-auto"
         initial={{ opacity: 0, x: -500 }} // Commence caché, décalé vers la gauche
         whileInView={{ opacity: 1, x: 0 }} // Devient visible en se déplaçant vers la droite
         transition={{ duration: 0.8, ease: "easeOut" }} // Animation fluide
         viewport={{ once: true, amount: 0.2 }} // Déclenchement à 20% de la div visible
-        >
-          <h3 className='m-auto font-poppins font-bold text-2xl'>Certification Full-Stack Developper</h3>
-          <div className='flex flex-row gap-4 flex-wrap'>
-             <h4>Développement Web Front-End</h4>
-             <p>Langages : HTML5 | CSS3 | JavaScript CMS : Wordpress</p>  
-             <p>Librairie : React </p>      
-             <h4>Développement Web Back-End</h4>
-             <p>Langages : Python</p>  
-             <p>Framework / Plateforme : Flask | Firebase </p>
-             <p>Outils et techniques de développement : DevOps | Git</p>
-             <p>Structure de la formation</p>
-             <p> La formation de Full-Stack Developer se déroule sur une durée d’un semestre.</p>
-            <p>Elle est basée sur la conception et le développement de travaux de projets personnels lors de chaque module.</p> 
-            <p>Le Diplôme de Full-Stack Developer représente 800 heures de travail correspondant à 32 ECTS (European Credits Transfert System).</p>                   
-          </div>          
-        </motion.div>
-        <motion.div
-        className="flex flex-wrap flex-col gap-4 p-8 rounded-[15px] border border-solid border-white w-[75%] ml-auto"
-        initial={{ opacity: 0, x: 500 }} // Commence caché, décalé vers la gauche
-        whileInView={{ opacity: 1, x: 0 }} // Devient visible en se déplaçant vers la droite
+      >
+        <h3 className="m-auto font-poppins font-bold text-2xl text-center lg:text-left">
+          Certification Full-Stack Developper
+        </h3>
+        <div className="flex flex-col gap-4">
+          <h4 className="font-poppins font-semibold text-xl">Développement Web Front-End</h4>
+          <p className="font-poppins text-base">
+            Langages : HTML5 | CSS3 | JavaScript CMS : Wordpress
+          </p>
+          <p className="font-poppins text-base">Librairie : React</p>
+          <h4 className="font-poppins font-semibold text-xl">Développement Web Back-End</h4>
+          <p className="font-poppins text-base">Langages : Python</p>
+          <p className="font-poppins text-base">
+            Framework / Plateforme : Flask | Firebase
+          </p>
+          <p className="font-poppins text-base">
+            Outils et techniques de développement : DevOps | Git
+          </p>
+          <p className="font-poppins text-base">Structure de la formation</p>
+          <p className="font-poppins text-base">
+            La formation de Full-Stack Developer se déroule sur une durée d’un
+            semestre.
+          </p>
+          <p className="font-poppins text-base">
+            Elle est basée sur la conception et le développement de travaux de
+            projets personnels lors de chaque module.
+          </p>
+          <p className="font-poppins text-base">
+            Le Diplôme de Full-Stack Developer représente 800 heures de travail
+            correspondant à 32 ECTS (European Credits Transfert System).
+          </p>
+        </div>
+      </motion.div>
+
+      {/* Deuxième certification */}
+      <motion.div
+        className="flex flex-col gap-4 p-4 lg:p-8 rounded-[15px] border border-solid border-white w-full lg:w-[75%] ml-auto"
+        initial={{ opacity: 0, x: 500 }} // Commence caché, décalé vers la droite
+        whileInView={{ opacity: 1, x: 0 }} // Devient visible en se déplaçant vers la gauche
         transition={{ duration: 0.8, ease: "easeOut" }} // Animation fluide
         viewport={{ once: true, amount: 0.2 }} // Déclenchement à 20% de la div visible
-        >
-          <h3 className='m-auto font-poppins font-bold text-2xl'>Certification Python Software Engineer</h3>
-          <div className='flex flex-row gap-4 flex-wrap'>             
-            <p>Travail de conception, de développement et d’implémentation d’une application back-end réalisée avec le langage de programmation Python. </p> 
-            <p>La durée totale de la formation est de huit semaines, dont deux semaines dédiées au travail de projet personnel.</p>                   
-          </div>          
-        </motion.div>
-        <motion.div
-        className="flex flex-wrap flex-col gap-4 p-8 rounded-[15px] border border-solid border-white w-[75%] "
+      >
+        <h3 className="m-auto font-poppins font-bold text-2xl text-center lg:text-left">
+          Certification Python Software Engineer
+        </h3>
+        <div className="flex flex-col gap-4">
+          <p className="font-poppins text-base">
+            Travail de conception, de développement et d’implémentation d’une
+            application back-end réalisée avec le langage de programmation
+            Python.
+          </p>
+          <p className="font-poppins text-base">
+            La durée totale de la formation est de huit semaines, dont deux
+            semaines dédiées au travail de projet personnel.
+          </p>
+        </div>
+      </motion.div>
+
+      {/* Troisième certification */}
+      <motion.div
+        className="flex flex-col gap-4 p-4 lg:p-8 rounded-[15px] border border-solid border-white w-full lg:w-[75%] mx-auto"
         initial={{ opacity: 0, x: -500 }} // Commence caché, décalé vers la gauche
         whileInView={{ opacity: 1, x: 0 }} // Devient visible en se déplaçant vers la droite
         transition={{ duration: 0.8, ease: "easeOut" }} // Animation fluide
         viewport={{ once: true, amount: 0.2 }} // Déclenchement à 20% de la div visible
-        >
-          <h3 className='m-auto font-poppins font-bold text-2xl'>Certification WebProgrammer</h3>
-          <div className='flex flex-row gap-4 flex-wrap'>             
-            <p>Travail de conception, de développement et d’implémentation d’une application web réalisée avec JavaScript ES6, le qualifiant pour le métier de WebProgrammer. </p> 
-            <p>Ce projet comporte un travail de conception, incluant l’analyse des besoins du client, l’architecture de l’information, la conception visuelle et le design de l’interface. La durée totale de la formation est de huit semaines, dont deux semaines dédiées au travail de projet personnel.</p>                   
-          </div>          
-        </motion.div>
-        <motion.div
-        className="flex flex-wrap flex-col gap-4 p-8 rounded-[15px] border border-solid border-white w-[75%] ml-auto"
-        initial={{ opacity: 0, x: 500 }} // Commence caché, décalé vers la gauche
-        whileInView={{ opacity: 1, x: 0 }} // Devient visible en se déplaçant vers la droite
+      >
+        <h3 className="m-auto font-poppins font-bold text-2xl text-center lg:text-left">
+          Certification WebProgrammer
+        </h3>
+        <div className="flex flex-col gap-4">
+          <p className="font-poppins text-base">
+            Travail de conception, de développement et d’implémentation d’une
+            application web réalisée avec JavaScript ES6, le qualifiant pour le
+            métier de WebProgrammer.
+          </p>
+          <p className="font-poppins text-base">
+            Ce projet comporte un travail de conception, incluant l’analyse des
+            besoins du client, l’architecture de l’information, la conception
+            visuelle et le design de l’interface. La durée totale de la formation
+            est de huit semaines, dont deux semaines dédiées au travail de projet
+            personnel.
+          </p>
+        </div>
+      </motion.div>
+
+      {/* Quatrième certification */}
+      <motion.div
+        className="flex flex-col gap-4 p-4 lg:p-8 rounded-[15px] border border-solid border-white w-full lg:w-[75%] ml-auto"
+        initial={{ opacity: 0, x: 500 }} // Commence caché, décalé vers la droite
+        whileInView={{ opacity: 1, x: 0 }} // Devient visible en se déplaçant vers la gauche
         transition={{ duration: 0.8, ease: "easeOut" }} // Animation fluide
         viewport={{ once: true, amount: 0.2 }} // Déclenchement à 20% de la div visible
-        >
-          <h3 className='m-auto font-poppins font-bold text-2xl'>Certification WebDesigner</h3>
-          <div className='flex flex-row gap-4 flex-wrap'>             
-            <p>Le travail correspondant au site comporte la conception d’un projet, incluant l’analyse du besoin du client, la gestion des contenus, l’architecture de l’information, la conception visuelle et le développement du site web.</p> 
-            <p>Ce projet comporte un travail de conception, incluant l’analyse des besoins du client, l’architecture de l’information, la conception visuelle et le design de l’interface. La durée totale de la formation est de huit semaines, dont deux semaines dédiées au travail de projet personnel.</p>
-            <p>La durée totale de la formation est de sept semaines, dont deux semaines dédiées au travail de projet personnel.</p>                   
-          </div>          
-        </motion.div>
-      </>
-    )
+      >
+        <h3 className="m-auto font-poppins font-bold text-2xl text-center lg:text-left">
+          Certification WebDesigner
+        </h3>
+        <div className="flex flex-col gap-4">
+          <p className="font-poppins text-base">
+            Le travail correspondant au site comporte la conception d’un projet,
+            incluant l’analyse du besoin du client, la gestion des contenus,
+            l’architecture de l’information, la conception visuelle et le
+            développement du site web.
+          </p>
+          <p className="font-poppins text-base">
+            Ce projet comporte un travail de conception, incluant l’analyse des
+            besoins du client, l’architecture de l’information, la conception
+            visuelle et le design de l’interface. La durée totale de la formation
+            est de huit semaines, dont deux semaines dédiées au travail de projet
+            personnel.
+          </p>
+          <p className="font-poppins text-base">
+            La durée totale de la formation est de sept semaines, dont deux
+            semaines dédiées au travail de projet personnel.
+          </p>
+        </div>
+      </motion.div>
+    </div>
+  );
 }
-export default Certifications
+
+export default Certifications;
--- src/components/contact.jsx
+++ src/components/contact.jsx
@@ -1,16 +1,97 @@
 import phone from '../assets/svg/contact_phone.svg';
+import mail from '../assets/svg/mail.svg';
+import linkedin from '../assets/svg/linkedin.svg';
+import github from '../assets/svg/github.svg';
+import Card from './card';
+import { motion } from 'framer-motion';
 
 export function Contact() {
     return (
-        <div className="flex flex-col gap-4 p-8 rounded-[15px] border border-solid border-white w-full md:w-1/2 lg:w-2/5">
-            <h2 className="text-center font-poppins font-bold text-2xl">Contact</h2>
-            <div className="flex flex-col gap-4">
-                <h3>Vincent Gaillard</h3>
-                <span><img src={phone}/>+33681931168</span>
-                <p className="font-poppins">
-                    Vous pouvez me contacter par mail à l'adresse suivante : <a href="mailto:vgaillard85@gmail.com">vgaillard85@gmail.com</a></p>
+        <div className="flex flex-col items-center gap-0">
+            {/* Section Contact */}
+            <h2 className="uppercase m-auto font-poppins font-bold text-2xl lg:text-[30px] mt-32">Contact</h2>
+            <div className="my-8 flex flex-col lg:flex-row items-center mt-32">
+                {/* Contenu de contact à gauche */}
+                <motion.div
+                    className="relative flex flex-col rounded-[15px] p-8 gap-4 w-full lg:max-w-[50%] h-auto"
+                    style={{ border: "1px solid transparent" }}
+                    
+                >
+                    {/* Animation de la bordure */}
+                    <motion.div
+                        className="absolute inset-0 rounded-[15px] border border-white"
+                        initial={{
+                            clipPath: "polygon(100% 50%, 100% 50%, 100% 50%, 100% 50%)",
+                        }}
+                        style={{ pointerEvents: "none" }}
+                        animate={{
+                            clipPath: "polygon(100% 0, 100% 100%, 0 100%, 0 0)",
+                        }}
+                        transition={{ duration: 1.5, ease: "easeOut", delay: 0.5 }}
+                    ></motion.div>
+
+                    <div className="flex flex-col gap-4 p-8">
+                        <h2 className="font-poppins font-bold text-2xl lg:text-[38px] mb-4">
+                            Vincent Gaillard
+                        </h2>
+                        <div className="flex items-center gap-2">
+                            <img src={phone} alt="Phone" className="w-6 h-6" />
+                            <span className="font-poppins text-xl lg:text-[28px]">+33681931168</span>
+                        </div>
+                        <a href="mailto:vgaillard85@gmail.com" className="flex items-center gap-2">
+                            <img src={mail} alt="Mail" className="w-6 h-6" />
+                            <span className="font-poppins text-xl lg:text-[28px]">vgaillard85@gmail.com</span>
+                        </a>
+                        <div className='flex flex-row justify-center gap-4'>
+                            <a href="https://www.linkedin.com/in/vincent-gaillard-164776287" target="_blank" rel="noopener noreferrer">
+                                <img src={linkedin} alt="LinkedIn" className='h-[100px]' />
+                            </a>
+                            <a href="https://github.com/vince8574" target="_blank" rel="noopener noreferrer">
+                                <img src={github} alt="GitHub" className='h-[100px]' />
+                            </a>
+                        </div>
+                    </div>
+                </motion.div>
+
+                {/* Ligne animée */}
+                <motion.div
+                    className="flex justify-center items-center"
+                    initial={{ opacity: 0 }}
+                    animate={{ opacity: 1 }}
+                    transition={{ duration: 1, delay: 2 }}
+                >
+                    {/* Ligne horizontale (grands écrans) */}
+                    <motion.div
+                        className="hidden lg:block bg-white border-t border-white"
+                        style={{ height: "1px", width: "420px" }}
+                        initial={{ scaleX: 0, transformOrigin: "left" }}
+                        animate={{ scaleX: 1 }}
+                        transition={{ duration: 1, ease: "easeOut", delay: 2 }}
+                    ></motion.div>
+
+                    {/* Ligne verticale (petits écrans) */}
+                    <motion.div
+                        className="block lg:hidden bg-white border-l border-white"
+                        style={{ width: "1px", height: "64px" }}
+                        initial={{ scaleY: 0, transformOrigin: "top" }}
+                        animate={{ scaleY: 1 }}
+                        transition={{ duration: 1, ease: "easeOut", delay: 2 }}
+                    ></motion.div>
+                </motion.div>
+
+                {/* Carte à droite */}
+                <motion.div
+                    className="w-full lg:w-1/2"
+                    initial={{ opacity: 0, x: 50 }}
+                    animate={{ opacity: 1, x: 0 }}
+                    transition={{ duration: 1, delay: 1 }}
+                >
+                    <Card />
+                </motion.div>
             </div>
+            
         </div>
     );
 }
+
 export default Contact;
