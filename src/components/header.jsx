520747c7a43c180f323a5411dd5d65200e324eb4
Vincent Gaillard <vgaillard85@gmail.com>
2025-02-04 21:43:55 +0100

    ctd

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,12 +1,12 @@
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 import { Link } from "react-router-dom";
 import download from "../assets/svg/download.svg";
 import cv from "../assets/pdf/cv.pdf";
 
-
 function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
   const [isScrolled, setIsScrolled] = useState(false);
+  const menuRef = useRef(null); // Référence pour le menu
 
   useEffect(() => {
     const handleScroll = () => {
@@ -16,6 +16,25 @@ function Header() {
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
   }, []);
+
+  useEffect(() => {
+    const handleClickOutside = (event) => {
+      // Fermer le menu si le clic est en dehors du menu
+      if (menuRef.current && !menuRef.current.contains(event.target)) {
+        setIsMenuOpen(false);
+      }
+    };
+
+    // Ajouter l'écouteur d'événement si le menu est ouvert
+    if (isMenuOpen) {
+      document.addEventListener("mousedown", handleClickOutside);
+    }
+
+    // Nettoyer l'écouteur d'événement
+    return () => {
+      document.removeEventListener("mousedown", handleClickOutside);
+    };
+  }, [isMenuOpen]); // Déclencher cet effet uniquement lorsque isMenuOpen change
 
   const toggleMenu = () => {
     setIsMenuOpen(!isMenuOpen);
@@ -34,8 +53,14 @@ function Header() {
       <nav className="h-fit justify-between w-full">
         {/* Bouton Burger Menu (Mobile) */}
         <div className="flex justify-between items-center px-4 lg:hidden">
-          <Link to="" title="Accueil" className="font-poppins font-bold text-lg"  onClick={() => {
-      window.scrollTo({top: 0, behavior: 'smooth'});}}>
+          <Link
+            to=""
+            title="Accueil"
+            className="font-poppins font-bold text-lg"
+            onClick={() => {
+              window.scrollTo({ top: 0, behavior: "smooth" });
+            }}
+          >
             Vincent Gaillard
           </Link>
           <button
@@ -44,12 +69,32 @@ function Header() {
             aria-label="Toggle Menu"
           >
             {isMenuOpen ? (
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M6 18L18 6M6 6l12 12" />
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M6 18L18 6M6 6l12 12"
+                />
               </svg>
             ) : (
-              <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" />
+              <svg
+                className="w-6 h-6"
+                fill="none"
+                stroke="currentColor"
+                viewBox="0 0 24 24"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="2"
+                  d="M4 6h16M4 12h16m-7 6h7"
+                />
               </svg>
             )}
           </button>
@@ -57,27 +102,68 @@ function Header() {
 
         {/* Menu principal */}
         <ul
+          ref={menuRef} // Ajouter une référence au menu
           className={`lg:flex lg:gap-4 lg:justify-center lg:items-center ${
             isMenuOpen ? "block" : "hidden"
           } lg:block absolute top-full left-0 w-full lg:static bg-[#1C2321] lg:bg-transparent`}
         >
           <li>
-            <Link 
-              to="" 
-              className="block font-poppins font-bold text-lg md:text-xl lg:text-2xl py-2 px-4 hover:bg-[#2a3432]" 
-              onClick={() => { 
-                closeMenu(); 
-                window.scrollTo({top: 0, behavior: "smooth"});
+            <Link
+              to=""
+              className="block font-poppins font-bold text-lg md:text-xl lg:text-2xl py-2 px-4 hover:bg-[#2a3432]"
+              onClick={() => {
+                closeMenu();
+                window.scrollTo({ top: 0, behavior: "smooth" });
               }}
             >
               Accueil
             </Link>
           </li>
-          <li><Link to="/#competences" className="block font-poppins font-bold text-lg md:text-xl lg:text-2xl py-2 px-4 hover:bg-[#2a3432]" onClick={closeMenu}>Compétences</Link></li>
-          <li><Link to="/#portfolio" className="block font-poppins font-bold text-lg md:text-xl lg:text-2xl py-2 px-4 hover:bg-[#2a3432]" onClick={closeMenu}>Portfolio</Link></li>
-          <li><Link to="/#experience" className="block font-poppins font-bold text-lg md:text-xl lg:text-2xl py-2 px-4 hover:bg-[#2a3432]" onClick={closeMenu}>Mon expérience</Link></li>
-          <li><Link to="/#merci" className="block font-poppins font-bold text-lg md:text-xl lg:text-2xl py-2 px-4 hover:bg-[#2a3432]" onClick={closeMenu}>Merci</Link></li>
-          <li><Link to="/contact" className="block font-poppins font-bold text-lg md:text-xl lg:text-2xl py-2 px-4 hover:bg-[#2a3432]" onClick={closeMenu}>Contact</Link></li>
+          <li>
+            <Link
+              to="/#competences"
+              className="block font-poppins font-bold text-lg md:text-xl lg:text-2xl py-2 px-4 hover:bg-[#2a3432]"
+              onClick={closeMenu}
+            >
+              Compétences
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/#portfolio"
+              className="block font-poppins font-bold text-lg md:text-xl lg:text-2xl py-2 px-4 hover:bg-[#2a3432]"
+              onClick={closeMenu}
+            >
+              Portfolio
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/#experience"
+              className="block font-poppins font-bold text-lg md:text-xl lg:text-2xl py-2 px-4 hover:bg-[#2a3432]"
+              onClick={closeMenu}
+            >
+              Mon expérience
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/#merci"
+              className="block font-poppins font-bold text-lg md:text-xl lg:text-2xl py-2 px-4 hover:bg-[#2a3432]"
+              onClick={closeMenu}
+            >
+              Merci
+            </Link>
+          </li>
+          <li>
+            <Link
+              to="/contact"
+              className="block font-poppins font-bold text-lg md:text-xl lg:text-2xl py-2 px-4 hover:bg-[#2a3432]"
+              onClick={closeMenu}
+            >
+              Contact
+            </Link>
+          </li>
           <li>
             <a
               className="flex flex-row font-poppins font-bold text-lg md:text-xl lg:text-2xl py-2 px-4 hover:bg-[#2a3432] items-center"
@@ -86,7 +172,12 @@ function Header() {
               title="CV"
               onClick={closeMenu}
             >
-              CV <img className="w-6 h-6 ml-2" src={download} alt="Télécharger le CV" />
+              CV{" "}
+              <img
+                className="w-6 h-6 ml-2"
+                src={download}
+                alt="Télécharger le CV"
+              />
             </a>
           </li>
         </ul>
