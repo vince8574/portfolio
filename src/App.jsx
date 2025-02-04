50807266b193c4c6cafe0404d9215968fbf8d4bd
Vincent Gaillard <vgaillard85@gmail.com>
2025-02-04 13:13:58 +0100

    changement des routes

--- src/App.jsx
+++ src/App.jsx
@@ -9,26 +9,20 @@ import Certifications from "./components/certifications";
 
 function App() {
   return (
-    <Router>
-      <div className="bg-complex-gradient min-h-screen flex flex-col">
-       
-        <Header />
-
-       
-        <div className="px-4 md:px-16 lg:px-32 xl:px-64 flex-grow mb-16" id="root">
-          <Routes>
-            <Route path="" element={<Home />} />
-            <Route path="contact" element={<Contact />} />
-            <Route path="portfolio" element={<Portfolio />} />
-            <Route path="certifications" element={<Certifications />} />
-            
-          </Routes>
-        </div>
-
-        
-        <Footer />
-      </div>
-    </Router>
+    <Router basename="/portfolio">
+  <div className="bg-complex-gradient min-h-screen flex flex-col">
+    <Header />
+    <div className="px-4 md:px-16 lg:px-32 xl:px-64 flex-grow mb-16" id="root">
+      <Routes>
+        <Route path="" element={<Home />} />
+        <Route path="contact" element={<Contact />} />
+        <Route path="portfolio" element={<Portfolio />} />
+        <Route path="certifications" element={<Certifications />} />
+      </Routes>
+    </div>
+    <Footer />
+  </div>
+</Router>
   );
 }
 
